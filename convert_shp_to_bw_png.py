342842c5bfe0753a0ad5ccb56924be240997b54a
menisadi <menisadi@gmail.com>
2023-10-24 21:50:57 +0300

    added usa

--- convert_shp_to_bw_png.py
+++ convert_shp_to_bw_png.py
@@ -2,9 +2,10 @@ import sys
 
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
-def convert_shp_to_bw_png(path_to_shapefile, resulting_name):
+def convert_shp_to_bw_png(path_to_shapefile, resulting_name, chosen_dpi=1000):
     """
     This function converts a shapefile to a black and white png image.
     :param path_to_shapefile: path to the shapefile
@@ -15,18 +16,19 @@ def convert_shp_to_bw_png(path_to_shapefile, resulting_name):
     gdf = gpd.read_file(path_to_shapefile)
 
     # Create png image
-    fig, ax = plt.subplots(figsize=(4, 4))
+    fig, ax = plt.subplots()
     gdf.plot(ax=ax)
 
     ax.axis("off")
 
     plt.savefig(
-        resulting_name + ".png", dpi=300, bbox_inches="tight", pad_inches=0
+        resulting_name + ".png",
+        dpi=chosen_dpi,
+        bbox_inches="tight",
+        pad_inches=0,
     )
 
     # convert the image to black and white
-    from PIL import Image
-
     img = Image.open(resulting_name + ".png")
     gray = img.convert("L")
     bw = gray.point(lambda x: 0 if x < 128 else 255, "1")
@@ -35,4 +37,7 @@ def convert_shp_to_bw_png(path_to_shapefile, resulting_name):
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    convert_shp_to_bw_png(args[0], args[1])
+    chosen_dpi = 1000
+    if len(args) == 3:
+        chosen_dpi = int(args[2])
+    convert_shp_to_bw_png(args[0], args[1], chosen_dpi)
