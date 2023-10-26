8946902ba6b6c201161a77e03a8bb5b60aca6f1a
menisadi <menisadi@gmail.com>
2023-10-26 23:35:00 +0300

    added dpi parameter

--- find_medial_axis.py
+++ find_medial_axis.py
@@ -5,7 +5,7 @@ import skimage
 from skimage.morphology import medial_axis
 
 
-def find_ma(path_to_bw_image):
+def find_ma(path_to_bw_image, result_dpi=1000):
     bw_mesh = skimage.io.imread(path_to_bw_image)
     medial_axis_mesh = medial_axis(skimage.util.invert(bw_mesh))
 
@@ -13,7 +13,7 @@ def find_ma(path_to_bw_image):
 
     plt.imshow(combined_mesh, cmap=plt.cm.gray)
     plt.axis("off")
-    plt.savefig("medial_axis.png", bbox_inches="tight")
+    plt.savefig("medial_axis.png", bbox_inches="tight", dpi=result_dpi)
 
 
 if __name__ == "__main__":
